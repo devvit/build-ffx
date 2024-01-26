f7da7719caaf755f7e2c2a51a68d8cf071b61ef2
ymca <admin@example.com>
2024-01-26 22:40:37 +0800

    Fri Jan 26 22:40:35 CST 2024  M .github/workflows/macos_amd64.yaml  M patch.sh

--- patch.sh
+++ patch.sh
@@ -50,7 +50,7 @@ else
     echo 'export MOZ_LTO=cross' >>mozconfig
 fi
 
-version=$(bash ../version.sh | tr 'a-z' 'A-Z' | tr -dc 'A-Z-')
-echo "MOZ_APP_DISPLAYNAME='Firefox ${version}'" >./browser/branding/unofficial/configure.sh
+version_name=$(echo $version | tr 'a-z' 'A-Z' | tr -dc 'A-Z-')
+echo "MOZ_APP_DISPLAYNAME='Firefox ${version_name}'" >./browser/branding/unofficial/configure.sh
 
 perl -pi -e 's/debuggerStatement\(\) {/debuggerStatement\(\) {return null\(\);/g' ./js/src/frontend/Parser.cpp
