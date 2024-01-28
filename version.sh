8aeff80276b0f54c5f58520d3ff23b06bf308e79
ymca <admin@example.com>
2024-01-28 15:18:43 +0800

    Sun Jan 28 15:18:41 CST 2024  M .github/workflows/macos_amd64.yaml  M version.sh

--- version.sh
+++ version.sh
@@ -3,7 +3,7 @@
 month_num=$(date +%m)
 month=$(expr $month_num % 2)
 if [ $month -eq 0 ]; then
-    curl -fsSL https://hg.mozilla.org/mozilla-unified/bookmarks | perl -ne 'printf("%s\n", $&) if /esr(\d+)/' | head -n1
-else
     echo 'beta'
+else
+    curl -fsSL https://hg.mozilla.org/mozilla-unified/bookmarks | perl -ne 'printf("%s\n", $&) if /esr(\d+)/' | head -n1
 fi
