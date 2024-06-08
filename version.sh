7ebabac26de3d5cec4da56d91ca890465ec65ace
devvit <1715224+devvit@users.noreply.github.com>
2024-06-08 11:40:11 +0800

    Update version.sh

--- version.sh
+++ version.sh
@@ -3,7 +3,7 @@
 month_num=$(date +%m)
 month=$(expr $month_num % 2)
 if [ $month -eq 0 ]; then
-    echo 'beta'
+    echo 'release'
 else
     curl -fsSL https://hg.mozilla.org/mozilla-unified/bookmarks | perl -ne 'printf("%s\n", $&) if /esr(\d+)/' | head -n1
 fi
