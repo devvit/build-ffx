a01f356c6d65f84fac42b452d6cf685117a97444
admin <admin@example.com>
2024-06-22 13:25:32 +0800

    Sat Jun 22 13:25:31 CST 2024  M .github/workflows/macos_amd64.yaml  M version.sh

--- version.sh
+++ version.sh
@@ -3,7 +3,7 @@
 month_num=$(date +%m)
 month=$(expr $month_num % 2)
 if [ $month -eq 0 ]; then
-    curl -fsSL https://hg.mozilla.org/mozilla-unified/bookmarks | perl -ne 'printf("%s\n", $&) if /esr(\d{3})/' | sort | tail -n1
-else
     echo 'release'
+else
+    curl -fsSL https://hg.mozilla.org/mozilla-unified/bookmarks | perl -ne 'printf("%s\n", $&) if /esr(\d{3})/' | sort | tail -n1
 fi
