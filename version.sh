a6241cac9542d99843fb02bbb8ae69f667e8d6aa
admin <admin@example.com>
2025-05-24 20:26:32 +0800

    Sat May 24 20:26:31 CST 2025  M .github/workflows/linux_amd64.yaml  M .github/workflows/macos_amd64.yaml  M .github/workflows/macos_arm64.yaml  M .github/workflows/win64_amd64.yaml  M version.sh

--- version.sh
+++ version.sh
@@ -4,7 +4,7 @@ month_num=$(date +%m)
 month=$(expr $month_num % 2)
 if [ $month -eq 0 ]; then
     # echo 'release'
-    curl -fsSL https://hg.mozilla.org/mozilla-unified/bookmarks | perl -ne 'printf("%s\n", $&) if /esr(\d{3})/' | sort | tail -n1
+    curl -fsSL https://api.github.com/repos/mozilla-firefox/firefox/branches | perl -ne 'printf("%s\n", $&) if /esr(\d+)/' | tail -n1
 else
-    curl -fsSL https://hg.mozilla.org/mozilla-unified/bookmarks | perl -ne 'printf("%s\n", $&) if /esr(\d{3})/' | sort | tail -n1
+    curl -fsSL https://api.github.com/repos/mozilla-firefox/firefox/branches | perl -ne 'printf("%s\n", $&) if /esr(\d+)/' | tail -n1
 fi
