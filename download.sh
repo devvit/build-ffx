696370306572c3f8b752a0f302f1168f465c4944
ymca <ymca@ymcas-Mac-Pro.lan>
2025-12-03 08:41:30 +0800

    Wed Dec  3 08:41:29 CST 2025  M .github/workflows/macos_amd64.yaml RM .github/workflows/download.sh -> download.sh

---
+++ download.sh
@@ -0,0 +1,9 @@
+#
+
+version=$1
+curl --retry 10 --retry-delay 5 --retry-all-errors \
+    --connect-timeout 30 --max-time 300 \
+    -fSL -k -JO -# "https://github.com/mozilla-firefox/firefox/archive/refs/heads/$version.zip"
+unzip -q firefox*.zip
+rm -rf firefox*.zip
+mv firefox* mozilla-unified
