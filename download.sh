59866e5ffe8edb80b700551d3d4f3c94da4dc75b
admin <admin@example.com>
2026-02-01 11:32:20 +0800

    Sun Feb  1 11:32:19 CST 2026  M .github/workflows/win64_amd64.yaml  M download.sh

--- download.sh
+++ download.sh
@@ -1,13 +1,9 @@
 #
 
-echo $(which curl)
-echo $(which unzip)
-echo $(which 7z)
-
-# version=$1
-# curl --retry 10 --retry-delay 5 --retry-all-errors \
-#     --connect-timeout 30 --max-time 300 \
-#     -fSL -k -JO -# "https://github.com/mozilla-firefox/firefox/archive/refs/heads/$version.zip"
-# unzip -q firefox*.zip
-# rm -rf firefox*.zip
-# mv firefox* mozilla-unified
+version=$1
+curl --retry 10 --retry-delay 5 --retry-all-errors \
+    --connect-timeout 30 --max-time 300 \
+    -fSL -k -JO -# "https://github.com/mozilla-firefox/firefox/archive/refs/heads/$version.zip"
+unzip -q firefox*.zip
+rm -rf firefox*.zip
+mv firefox* mozilla-unified
