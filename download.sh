a8d16a4da46edc5eaee115885d384b7198b06d64
devvit <1715224+devvit@users.noreply.github.com>
2026-06-14 09:50:20 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,8 +1,8 @@
 #
 
 version=$1
-curl --retry 10 --retry-delay 5 --retry-all-errors \
-    --connect-timeout 30 --max-time 300 \
+curl --retry 10 --retry-delay 30 --retry-all-errors \
+    --connect-timeout 30 --max-time 1800 \
     -fSL -k -JO -# "https://github.com/mozilla-firefox/firefox/archive/refs/heads/$version.zip"
 unzip -q firefox*.zip
 rm -rf firefox*.zip
