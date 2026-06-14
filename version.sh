d0bd3ac6a531c9259b6665715987681bcb96f8ab
admin <admin@example.com>
2026-06-14 10:34:19 +0800

    Sun Jun 14 10:34:18 CST 2026  M .github/workflows/macos_amd64.yaml  M .github/workflows/win64_amd64.yaml  M version.sh

--- version.sh
+++ version.sh
@@ -1,10 +1,11 @@
 #
 
-month_num=$(date +%m)
-month=$(expr $month_num % 2)
-if [ $month -eq 0 ]; then
-    # echo 'release'
-    curl -fsSL https://api.github.com/repos/mozilla-firefox/firefox/branches | perl -ne 'printf("%s\n", $&) if /esr(\d+)/' | tail -n1
-else
-    curl -fsSL https://api.github.com/repos/mozilla-firefox/firefox/branches | perl -ne 'printf("%s\n", $&) if /esr(\d+)/' | tail -n1
-fi
+# month_num=$(date +%m)
+# month=$(expr $month_num % 2)
+# if [ $month -eq 0 ]; then
+#     curl -fsSL https://api.github.com/repos/mozilla-firefox/firefox/branches | perl -ne 'printf("%s\n", $&) if /esr(\d+)/' | tail -n1
+# else
+#     curl -fsSL https://api.github.com/repos/mozilla-firefox/firefox/branches | perl -ne 'printf("%s\n", $&) if /esr(\d+)/' | tail -n1
+# fi
+
+echo 'esr140'
